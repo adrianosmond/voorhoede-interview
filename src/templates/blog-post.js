0db7d88f6517c94d46f09d354e0a15035225d19c
Adrian Osmond <adrian@Adrians-MacBook-Pro.local>
2018-01-20 20:14:45 +0100

    Syntax highlighting

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,7 @@
-import React from "react"
+import React, { Component } from "react"
 import Helmet from 'react-helmet'
 import marked from 'marked'
+import Prism from 'prismjs'
 import TimeToRead from '../components/TimeToRead'
 import './blog-post.css'
 import './prism.css'
@@ -9,17 +10,21 @@ const readingTime = (text) => {
   return Math.round(text.length / 2000)
 }
 
-export default ({ data }) => {
-  const post = data.allContentfulBlogPost.edges[0].node;
-  return (
-    <div className="article">
-      <Helmet title={post.title} />
-      <h1 className="article__title">{post.title}</h1>
-      <TimeToRead timeToRead={readingTime(post.content.content)} />
-      <article className="article__content" dangerouslySetInnerHTML={{ __html: marked(post.content.content) }} />
-    </div>
-  )
+class BlogPost extends Component {
+  render () {
+    const post = this.props.data.allContentfulBlogPost.edges[0].node;
+    return (
+      <div className="article">
+        <Helmet title={post.title} />
+        <h1 className="article__title">{post.title}</h1>
+        <TimeToRead timeToRead={readingTime(post.content.content)} />
+        <article className="article__content" dangerouslySetInnerHTML={{ __html: marked(post.content.content) }} />
+      </div>
+    )
+  }
 }
+
+export default BlogPost
 
 export const query = graphql`
   query BlogPostQuery($slug: String!) {
