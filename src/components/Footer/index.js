a853905db8a1d2f95f3d4299aa41d31930f3dd52
Adrian Osmond <adrian@Adrians-MacBook-Pro.local>
2018-01-21 14:24:34 +0100

    Changed logo colours Fixed contentful logo in FF

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,9 @@ import './index.css'
 const Footer = (props) => (
   <footer className="site-footer">
     <div className="site-footer__content">
-      <small className="site-footer__copyright">Copyright &copy; 2018, Front End Blog</small>
+      <small className="site-footer__copyright">Copyright &copy; 2018, {props.title}</small>
       <a className="site-footer__attribution" href="https://www.contentful.com/" rel="nofollow" target="_blank">
-        <img src="https://images.contentful.com/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg" style={{maxWidth:'100px', width:'100%'}} alt="Powered by Contentful" />
+        <img src="https://images.contentful.com/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg" style={{width:'100px'}} alt="Powered by Contentful" />
       </a>
     </div>
   </footer>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,11 +7,11 @@ const Header = (props) => (
     <nav className="site-header__nav">
       <Link to="/" className="site-header__link">
         <svg className="site-header__logo" viewBox="0 0 140 20" xmlns="http://www.w3.org/2000/svg">
-          <path d="M11.561 2.4l4.6 4.6-4.6 4.6 1.4 1.4 6-6-6-6-1.4 1.4z"/>
-          <path d="M11.298 8.389l4.6 4.6-4.6 4.6 1.4 1.4 6-6-6-6-1.4 1.4z"/>
-          <path d="M8.511 11.6L3.911 7l4.6-4.6-1.4-1.4-6 6 6 6 1.4-1.4z"/>
-          <path d="M8.249 17.589l-4.6-4.6 4.6-4.6-1.4-1.4-6 6 6 6 1.4-1.4z"/>
-          <text fontWeight="bold" fontSize="16" y="16" x="23">{props.title}</text>
+          <path className="site-header__logo-piece" d="M11.561 2.4l4.6 4.6-4.6 4.6 1.4 1.4 6-6-6-6-1.4 1.4z"/>
+          <path className="site-header__logo-piece" d="M11.298 8.389l4.6 4.6-4.6 4.6 1.4 1.4 6-6-6-6-1.4 1.4z"/>
+          <path className="site-header__logo-piece" d="M8.511 11.6L3.911 7l4.6-4.6-1.4-1.4-6 6 6 6 1.4-1.4z"/>
+          <path className="site-header__logo-piece" d="M8.249 17.589l-4.6-4.6 4.6-4.6-1.4-1.4-6 6 6 6 1.4-1.4z"/>
+          <text className="site-header__logo-text" fontWeight="bold" fontSize="16" y="16" x="23">{props.title}</text>
         </svg>
       </Link>
     </nav>
